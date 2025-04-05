bb83d3a6fe63865664e9dbc4f99d4c3a52a719fc
aryanmaurya1803 <aryankushwaha3245@gmail.com>
2025-04-05 08:00:39 +0530

    Update hello word.py

--- hello word.py
+++ hello word.py
@@ -1,12 +1,3 @@
-"""b=30
-a=20
-a=int(input("entre a number"))
-b=int(input("enter a second number"))
-print(a+b)"""
+print("hello word")
 
-a=int(input("enter a number"))
-i=1
-while(i>10):
- print(a*i)
- i=i-1
     
