5a36cd7eef9943b5737dfcfbb518b4dd9328c6fb
aryanmaurya1803 <aryankushwaha3245@gmail.com>
2025-04-10 07:26:47 +0530

    Add files via upload

---
+++ month.program.py
@@ -0,0 +1,83 @@
+'''for i in range(0,12):
+    a=int(input('enter a number you want'))
+    if(a==1):
+        print('january')
+    elif(a==2):
+        print('february')
+    elif(a==3):
+        print('march')
+    elif(a==4):
+        print('april')
+    elif(a==5):
+        print('may')
+    elif(a==6):
+        print('jun')
+    elif(a==7):
+        print('july')
+    elif(a==8):
+        print('august')
+    elif(a==9):
+        print('september')
+    elif(a==10):
+        print('ocotuber')
+    elif(a==11):
+        print('november')
+    elif(a==12):
+        print('december')
+    else:
+        print('error')'''
+
+'''a=int(input('enter a number'))
+if(a%2==0):
+   print('even')
+else:
+    pint('odd')
+a=int(input('enter a number1'))
+b=int(input('enter a number2'))
+if(a>b):
+    print('a is greater')
+elif(a<b):
+    print('b is greather')
+else:
+    print('error')'''
+
+
+
+l1=[]
+b=input('enter a number')
+for i in b:
+    d=int(input('enter a number'))
+    c=d**3
+    l1.append(c)
+print(l1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
